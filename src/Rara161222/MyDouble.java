344bb0ced0763ab129b9a94e5675df483dafbdba
RaraBisri <rarabisri@gmail.com>
2023-01-03 23:02:51 +0800

    Create MyDouble.java

---
+++ src/Rara161222/MyDouble.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rara161222;
+
+/**
+ *
+ * @author USER
+ */
+public class MyDouble  {
+    public boolean isGreater( Object a, Object b){
+        return (double)a > (double)b;
+    }
+    public boolean isLess( Object a, Object b){
+        return (double)a < (double)b;
+    }
+    
+    public boolean isEqual( Object a, Object b){
+        return (double)a == (double)b;
+    }  
+}
